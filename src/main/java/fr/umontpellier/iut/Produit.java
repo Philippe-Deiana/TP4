61f06903bc28bf0f37340a530055186fc54d9c46
Philippe <philippe.deiana@etu.umontpellier.fr>
2019-02-12 09:31:58 +0100

    class produit fait

---
+++ src/main/java/fr/umontpellier/iut/Produit.java
@@ -0,0 +1,43 @@
+package fr.umontpellier.iut;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Produit {
+
+    private int numero;
+    private String description;
+    private double prixCourant;
+    private LocalDate dateEnchere;
+    private LocalTime heureDebut;
+    private int lePasMini;
+    private int coutParticipation;
+    private boolean ouverture;
+
+    public Produit(int numero, String description, double prixCourant, int coutParticipation) {
+        this.numero = numero;
+        this.description = description;
+        this.prixCourant = prixCourant;
+        this.coutParticipation = coutParticipation;
+    }
+
+    public void setLePasMini(int lePasMini) {
+        this.lePasMini = lePasMini;
+    }
+
+    public void demarrerEnchere(){
+
+        this.heureDebut=LocalTime.now();
+        this.dateEnchere=LocalDate.now();
+        this.ouverture= true;
+
+
+    }
+
+    public void arreterEnchere(){
+        this.ouverture= false;
+
+    }
+
+
+}
